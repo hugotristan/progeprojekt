17cb2d89ff5c469ca37abc2f027209c00f2fa0e8
Mattias Rahnu <rahnumat@ut.ee>
2024-11-13 19:58:33 +0200

    jah

--- Keskendumise_hindaja-TOIMIV.py
+++ Keskendumise_hindaja-TOIMIV.py
@@ -1,9 +1,26 @@
+################################################
+# Programmeerimine I
+# 2024/2025 sügissemester
+#
+# Projekt Tuto
+# Teema: Keskendumise hindaja
+#
+#
+# Autorid: Mattias Rahnu, Hugo Tristan Tammik
+#
+# Eeskuju: Saadud inspiratsiooni face_recognitioni projektikirjelduses olevatest näidetest,
+#          loodud teemakohasemaks.
+#
+#Käivitamiseks:Vaja laadida C++ Build tools (installimisel vaja märkida desktop development, kõige ülemine ning vasakpoolseim)
+#              Laadida vaja face_recognition: pip install face-recognition cmd-s
+#              Laadida vaja ka opencv: pip install opencv-python
+#
+##################################################
+
+
 import face_recognition
 import cv2
-#import numpy as np
 
-#Vaja laadida C++ Build tools (installimisel vasakult kõige ülemine ära märkida)
-#laadida vaja face_recognition: pip install face-recognition cmd-s
 
 
 
@@ -15,11 +32,11 @@ face_locations = []
 kontrollitav_kaader = True
 
 # muutuja kasti (näo tuvastuskauguse) jaoks
-D = 0.5
-S = int(1/D)
+d = 0.5
+s = int(1/d)
 
 #kaadrite arv (mitme kaadri tagant kontrollida kohalolu)
-F = 1
+f = 1
 
 # p - mitu kaadrit oldi kohal | n - mitu kaadrit ei tuvastatud inimest
 p=0
@@ -30,7 +47,7 @@ while True:
 
     # vahelejäävad kaadrid
     i=0
-    while i < F:
+    while i < f:
         #ret on tagastusväärtus
         ret, kaader = kaamera.read()
         i+=1
@@ -38,11 +55,7 @@ while True:
     # kontrollitav kaader
     if kontrollitav_kaader:
         # kaadri väiksemaks tegemine näo kiiremaks tuvastamiseks
-        pisem_kaader = cv2.resize(kaader, (0, 0), fx=D, fy=D)
-        
-        # peaks olema ebaoluline
-        # Convert BGR color (OpenCV) to RGB color (face_recognition)
-        #rgb_small_kaader = np.array(small_kaader, dtype=np.uint8)
+        pisem_kaader = cv2.resize(kaader, (0, 0), fx=d, fy=d)
 
         # Näotuvastus - tuvastab ning lisab koordinaadid järjendisse face_locations
         face_locations = face_recognition.face_locations(pisem_kaader)
@@ -58,10 +71,10 @@ while True:
     # Teeb kasti
     for (ülemine, parem, alumine, vasak) in face_locations:
         # näo asukoha toob tagasi e kasti asukoha muudab õigeks
-        ülemine *= S
-        parem *= S
-        alumine *= S
-        vasak *= S
+        ülemine *= s
+        parem *= s
+        alumine *= s
+        vasak *= s
 
         cv2.rectangle(kaader, (vasak, ülemine), (parem, alumine), (0, 0, 255), 2)
 
@@ -69,8 +82,8 @@ while True:
     cv2.imshow('Video', kaader)
     
 
-    # Press 'q' to quit the video display
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # Vajutades q-d, lõpetab tegevuse
+    if cv2.waitKey(1) & 0xff == ord('q'):
         break
 
     
@@ -80,8 +93,8 @@ kaamera.release()
 cv2.destroyAllWindows()
 
 # arvutused
-print(p)
-print(n)
+print(p) #kaadrid, mil oldi kohal
+print(n) #kaadrid, millal ei oldud kohal
 if n != 0:
     print((p*100)/(p+n))
     if (p*100)/(p+n) > 80:
@@ -91,10 +104,4 @@ else:
     print("Keskendusid kogu aja!")
     
     
-    
-#20-20-20 reegel?
-#telefonituvastus YOLOv5-ga?
-#arvestada ajakulu ka sisse
-#3 asja, mis meelde jäid (kui lühike, siis küsib lähemalt)
-#saaks kuidagi loenguslaidid edastada ja siis küsib suvalt 3 asja??
     
